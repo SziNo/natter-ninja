b4d56fe79c262709a6126aae7186157471244519
SziNo <szino1848@gmail.com>
2023-05-13 12:38:35 +0200

    update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import ToasterContext from './context/ToasterContext'
-import AuthContext from './context/AuthContext'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import ToasterContext from './context/ToasterContext';
+import AuthContext from './context/AuthContext';
+import ActiveStatus from './components/ActiveStatus';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Natter Ninja',
   description: 'Natter Ninja',
-}
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
+          <ActiveStatus />
           {children}
         </AuthContext>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
