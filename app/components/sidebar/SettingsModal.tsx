8e390b02cfa99dc11447a1c6d79b761f6b1a9734
SziNo <szino1848@gmail.com>
2023-05-13 12:39:37 +0200

    remove console log

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -1,22 +1,22 @@
-'use client'
+'use client';
 
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
-import { User } from '@prisma/client'
-import { CldUploadButton } from 'next-cloudinary'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { User } from '@prisma/client';
+import { CldUploadButton } from 'next-cloudinary';
 
-import Input from '../inputs/Input'
-import Modal from '../modals/Modal'
-import Button from '../Button'
-import Image from 'next/image'
-import { toast } from 'react-hot-toast'
+import Input from '../inputs/Input';
+import Modal from '../modals/Modal';
+import Button from '../Button';
+import Image from 'next/image';
+import { toast } from 'react-hot-toast';
 
 interface SettingsModalProps {
-  isOpen?: boolean
-  onClose: () => void
-  currentUser: User
+  isOpen?: boolean;
+  onClose: () => void;
+  currentUser: User;
 }
 
 const SettingsModal: React.FC<SettingsModalProps> = ({
@@ -24,10 +24,8 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
   onClose,
   currentUser = {},
 }) => {
-  const router = useRouter()
-  const [isLoading, setIsLoading] = useState(false)
-
-  console.log(currentUser, '&TEST_CURRENT_USER')
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -40,86 +38,86 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
       name: currentUser?.name,
       image: currentUser?.image,
     },
-  })
+  });
 
-  const image = watch('image')
+  const image = watch('image');
 
   const handleUpload = (result: any) => {
     setValue('image', result.info.secure_url, {
       shouldValidate: true,
-    })
-  }
+    });
+  };
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     axios
       .post('/api/settings', data)
       .then(() => {
-        router.refresh()
-        onClose()
+        router.refresh();
+        onClose();
       })
       .catch(() => toast.error('Something went wrong!'))
-      .finally(() => setIsLoading(false))
-  }
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='space-y-12'>
-          <div className='border-b border-gray-900/10 pb-12'>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
             <h2
-              className='
+              className="
                 text-base 
                 font-semibold 
                 leading-7 
                 text-gray-900
-              '
+              "
             >
               Profile
             </h2>
-            <p className='mt-1 text-sm leading-6 text-gray-600'>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
               Edit your public information.
             </p>
 
-            <div className='mt-10 flex flex-col gap-y-8'>
+            <div className="mt-10 flex flex-col gap-y-8">
               <Input
                 disabled={isLoading}
-                label='Name'
-                id='name'
+                label="Name"
+                id="name"
                 errors={errors}
                 required
                 register={register}
               />
               <div>
                 <label
-                  htmlFor='photo'
-                  className='
+                  htmlFor="photo"
+                  className="
                     block 
                     text-sm 
                     font-medium 
                     leading-6 
                     text-gray-900
-                  '
+                  "
                 >
                   Photo
                 </label>
-                <div className='mt-2 flex items-center gap-x-3'>
+                <div className="mt-2 flex items-center gap-x-3">
                   <Image
-                    width='48'
-                    height='48'
-                    className='rounded-full'
+                    width="48"
+                    height="48"
+                    className="rounded-full"
                     src={
                       image || currentUser?.image || '/images/placeholder.jpg'
                     }
-                    alt='Avatar'
+                    alt="Avatar"
                   />
                   <CldUploadButton
                     options={{ maxFiles: 1 }}
                     onUpload={handleUpload}
-                    uploadPreset='khp8krl6'
+                    uploadPreset="khp8krl6"
                   >
-                    <Button disabled={isLoading} secondary type='button'>
+                    <Button disabled={isLoading} secondary type="button">
                       Change
                     </Button>
                   </CldUploadButton>
@@ -130,24 +128,24 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
         </div>
 
         <div
-          className='
+          className="
             mt-6 
             flex 
             items-center 
             justify-end 
             gap-x-6
-          '
+          "
         >
           <Button disabled={isLoading} secondary onClick={onClose}>
             Cancel
           </Button>
-          <Button disabled={isLoading} type='submit'>
+          <Button disabled={isLoading} type="submit">
             Save
           </Button>
         </div>
       </form>
     </Modal>
-  )
-}
+  );
+};
 
-export default SettingsModal
+export default SettingsModal;
